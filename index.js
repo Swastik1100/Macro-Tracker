bc7f684f17115493db1ca528433cf793f436ef3c
Swastik1100 <swastikpandey1100@gmail.com>
2026-04-15 02:59:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 const express    = require('express');
 const cors       = require('cors');
 const path       = require('path');
-const mealsRoute = require('./routes/meals');
+const mealsRoute = require('./meals');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
