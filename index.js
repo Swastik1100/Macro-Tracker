12c4a6ea35a2ddd39188c9db9b6468bac1913d5c
Swastik1100 <swastikpandey1100@gmail.com>
2026-04-15 02:54:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve the frontend from /public
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname)));
 
 // ── API Routes ───────────────────────────────
 app.use('/api', mealsRoute);
